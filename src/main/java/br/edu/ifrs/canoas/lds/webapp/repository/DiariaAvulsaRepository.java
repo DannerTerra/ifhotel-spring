4b78d67d76548cb0ab387c05fcea66ed61e3b2bb
Anderson Antunes <anderson.ant.oli@gmail.com>
2018-05-10 18:41:35 -0300

    .

--- src/main/java/br/edu/ifrs/canoas/lds/webapp/repository/DiariaAvulsaRepository.java
+++ src/main/java/br/edu/ifrs/canoas/lds/webapp/repository/DiariaAvulsaRepository.java
@@ -1,10 +1,14 @@
 package br.edu.ifrs.canoas.lds.webapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.edu.ifrs.canoas.lds.webapp.domain.DiariaAvulsa;
 @Repository
 public interface DiariaAvulsaRepository extends JpaRepository<DiariaAvulsa, Long> {
+
+	List<DiariaAvulsa> findByQuarto_Id(Long id);
 	
 }
